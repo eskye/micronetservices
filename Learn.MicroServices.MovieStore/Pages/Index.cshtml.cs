1feb0ea5b306c5ee961ae417a36042d94a3a8408
Sunkanmi Ijatuyi <sunkanmi.ijatuyi@silveredgeprojects.com>
2019-01-22 14:20:46 +0100

    Initial Commit

---
+++ Learn.MicroServices.MovieStore/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Learn.MicroServices.MovieStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HttpClient _client;
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("api");
+        }
+
+        public string[] Values { get; set; }
+        public async Task OnGet()
+        {
+            var response = await _client.GetAsync("/api/values");
+            var content = await response.Content.ReadAsStringAsync();
+            Values = JsonConvert.DeserializeObject<string[]>(content);
+        }
+    }
+}
